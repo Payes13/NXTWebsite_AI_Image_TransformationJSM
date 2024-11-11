dde50396fc1741a97f5e16f5236a22e0244b4656
Alexander Payés <66833788+Payes13@users.noreply.github.com>
2024-11-11 14:07:11 -0500

    Update page.tsx

--- app/(root)/credits/page.tsx
+++ app/(root)/credits/page.tsx
@@ -8,7 +8,6 @@ import { plans } from "@/constants";
 import { getUserById } from "@/lib/actions/user.actions";
 import Checkout from "@/components/shared/Checkout";
 
-// IN THIS PAGE WE PURCHASE CREDITS
 const Credits = async () => {
   const { userId } = auth();
 
